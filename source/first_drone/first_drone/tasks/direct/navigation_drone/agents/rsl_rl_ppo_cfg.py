6010541534634ee766937ea7bae0f3e36d533f40
fakesociety <powe3k@gmail.com>
2026-06-16 01:46:24 +0300

    Configure fine-tuning hyperparameters (entropy_coef=0.0001, lr=5e-5, max_iter=8000)

--- source/first_drone/first_drone/tasks/direct/navigation_drone/agents/rsl_rl_ppo_cfg.py
+++ source/first_drone/first_drone/tasks/direct/navigation_drone/agents/rsl_rl_ppo_cfg.py
@@ -4,7 +4,7 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlMLPModelCfg, RslRlPp
 @configclass
 class NavigationPPOCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
-    max_iterations = 6000
+    max_iterations = 8000
     save_interval = 50
     experiment_name = "navigation_drone_direct"
     logger = "wandb"
@@ -38,10 +38,10 @@ class NavigationPPOCfg(RslRlOnPolicyRunnerCfg):
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.0004,
+        entropy_coef=0.0001,
         num_learning_epochs=5,
         num_mini_batches=8,
-        learning_rate=1.0e-4,
+        learning_rate=5.0e-5,
         schedule="fixed",
         gamma=0.998,
         lam=0.95,
